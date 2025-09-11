0ddf2eb27db7afbf3c9dbd5f8e6b8271729e6e45
Abubakar Mi <abubakarmi131@gmail.com>
2025-09-11 11:10:04 +0100

    register

---
+++ .next/static/chunks/src_app_(auth)_login_page_tsx_1683893a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_f268c70e._.js",
+  "static/chunks/src_85236587._.js"
+],
+    source: "dynamic"
+});
